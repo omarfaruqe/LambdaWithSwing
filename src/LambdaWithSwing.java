8a22e6e122914ae002d47a88dca4efb615903c06
Omar Faruqe <faruqe.cse@gmail.com>
2021-12-29 16:04:24 +0600

    Lambda expression added to the button listener

---
+++ src/LambdaWithSwing.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LambdaWithSwing extends JFrame {
+    JLabel lblCountClicked;
+    JButton okButton;
+    int count;
+    LambdaWithSwing(){
+        count = 0;
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        okButton = new JButton("OK");
+        getContentPane().add(okButton);
+        okButton.addActionListener(e -> {
+                count = count + 1;
+                lblCountClicked.setText("Clicked " + count + " times");
+        });
+
+        lblCountClicked = new JLabel("Click count");
+        getContentPane().add(lblCountClicked);
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new LambdaWithSwing();
+            }
+        });
+    }
+}
